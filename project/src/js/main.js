cd62bb9e9f2842697b2ad3015072475d51c9db0f
Ilyuz <wingeddeath62@gmail.com>
2019-12-24 22:40:31 +0500

    feat: add yandex-map

--- project/src/js/main.js
+++ project/src/js/main.js
@@ -252,6 +252,44 @@ $(document).ready(function () {
 
     // маска для телефона
     $('[type=tel]').mask('+7(000) 000-00-00', { placeholder: "+7(___) ___-__-__"});
+
+
+
+
+   ymaps.ready(function () {
+     var myMap = new ymaps.Map('map', {
+         center: [55.751574, 37.573856],
+         zoom: 9
+       }, {
+         searchControlProvider: 'yandex#search'
+       }),
+
+       // Создаём макет содержимого.
+       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+         '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+       ),
+
+       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+         hintContent: 'Собственный значок метки',
+         balloonContent: 'Это красивая метка'
+       }, {
+         // Опции.
+         // Необходимо указать данный тип макета.
+         iconLayout: 'default#image',
+         // Своё изображение иконки метки.
+         iconImageHref: 'img/footer/placeholder.png',
+         // Размеры метки.
+         iconImageSize: [32, 32],
+         // Смещение левого верхнего угла иконки относительно
+         // её "ножки" (точки привязки).
+         iconImageOffset: [-5, -38]
+       });
+
+     myMap.geoObjects
+       .add(myPlacemark)
+       .add(myPlacemarkWithContent);
+   });
+
 });
 
 
@@ -274,3 +312,4 @@ $('#up').on('click', function (e) {
   }, '300');
 }); 
 
+  
